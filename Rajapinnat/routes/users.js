f403c155bee69989d5e0e96f17a8341109fd373c
jjauhiai <jukka.jauhiainen@oamk.fi>
2024-02-12 11:04:35 +0200

    tuntiesim. get post ei tietokantaa

--- Rajapinnat/routes/users.js
+++ Rajapinnat/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/example',
+    function(request,response){
+        response.send('I am example in /routes/users.js');
+        console.log('I am example');
+    }
+);
 module.exports = router;
